d4af712e8109d60a852d1ae0a19aba2ce1600b70
Anton Wijs <A.J.Wijs@tue.nl>
2021-01-07 11:10:46 +0100

    a

--- SLCOtoGPUexplore/tests/example_system_gpuexplore.cuh
+++ SLCOtoGPUexplore/tests/example_system_gpuexplore.cuh
@@ -2923,11 +2923,11 @@ inline __device__ uint64_t FINDORPUT_SINGLE(compressed_nodetype *d_q, nodetype *
 			addr = get_index_internal(e2);
 			element = d_q_i[addr];
 			if (element == EMPTY_NODE) {
-				element = atomicCAS((unsigned long long *) &(d_q_i[20]), EMPTY_NODE, (unsigned long long) node);
-				if (element == EMPTY_NODE) {
-					// Successfully stored the node.
+				element = atomicCAS((unsigned long long *) &(d_q_i[addr]), EMPTY_NODE, (unsigned long long) node);
+//				if (element == EMPTY_NODE) {
+//					// Successfully stored the node.
 					return addr;
-				}
+//				}
 			}
 //			if (element == node) {
 //				// The node is already stored.
