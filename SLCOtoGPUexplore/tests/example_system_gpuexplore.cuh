ff24413e6db3f9e590e9822d53c8c9875b59a828
Anton Wijs <A.J.Wijs@tue.nl>
2021-01-07 13:14:46 +0100

    a

--- SLCOtoGPUexplore/tests/example_system_gpuexplore.cuh
+++ SLCOtoGPUexplore/tests/example_system_gpuexplore.cuh
@@ -2926,7 +2926,7 @@ inline __device__ uint64_t FINDORPUT_SINGLE(compressed_nodetype *d_q, nodetype *
 				element2 = atomicCAS((unsigned long long *) &(d_q_i[addr]), (unsigned long long) element, (unsigned long long) node);
 				if (element == element2) {
 					// Successfully stored the node.
-					//d_q_i[1000] = 0;
+					d_q_i[1000] = 0;
 					return addr;
 				}
 			}
